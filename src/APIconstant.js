6f6a346b298fafa1ba29ec93d34d98ec9c822a4f
samwel2000 <samwelgfrey@gmail.com>
2021-07-30 21:39:48 -0500

    -

---
+++ src/APIconstant.js
@@ -0,0 +1,7 @@
+const BaseUrl = "localhost:8000/"
+
+const JobsListAPIEndpoint = "jobs-list/"
+const NewsListAPIEndpoint = "news-list/"
+const TeamMembersListAPIEndpoint = "team-members-list/"
+const ContactUsAPIEndpoint = "jobs-list/"
+
